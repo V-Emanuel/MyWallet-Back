7912f9c861f9a40bc9a2a88337f5a0c617f4ff70
V-Emanuel <gvictor567@gmail.com>
2023-01-19 16:02:29 -0300

    post /sing-up created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
-import cors from "cors";
-import { MongoClient} from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import cors from "cors";
+import joi from "joi";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from 'uuid';
 dotenv.config();
 
 const PORT = 5000;
@@ -22,4 +25,39 @@ try {
     console.log('Deu errado')
 }
 
-app.listen(PORT);
+app.post("/sing-up", async (req, res) => {
+    const user = req.body; //nome, email, senha, confirmeSenha
+    const passwordHash = bcrypt.hashSync(user.senha, 10);
+    const userSchema = joi.object({
+        nome: joi.string().required(),
+        email: joi.string().email().required(),
+        senha: joi.string().required(),
+        confirmeSenha: joi.ref(senha)
+    })
+    const validation = userSchema.validate(messages, { abortEarly: true });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors)
+    }
+    try {
+        const userExists = await db.colection('users').findOne({ email: user.email })
+        if (userExists) return res.status(400).send("Email já cadastrado")
+
+        await db.colection('users').insertOne({ ...user, senha: passwordHash })
+    } catch(err) {
+        res.status(500).send(err.message)
+    }
+})
+
+{/*app.post("/sign-in", async (req, res) => {
+    const user = res.body; // email, senha
+    await db.colection('users').insertOne(user)
+    const emailExist = await db.colection('users').findOne({ email: user.email })
+    if (emailExist && bcrypt.compareSync()) {
+
+    } else {
+
+    }
+})*/}
+
+app.listen(PORT, () => console.log('tudo certo, nada errado'));
